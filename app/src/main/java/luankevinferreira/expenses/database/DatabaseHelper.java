2c8805f01081213af97aed73fde83ed81cb9989d
luankevinferreira <luankevinferreira@gmail.com>
2016-07-09 13:00:59 -0300

    Add database file.

---
+++ app/src/main/java/luankevinferreira/expenses/database/DatabaseHelper.java
@@ -0,0 +1,42 @@
+package luankevinferreira.expenses.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static luankevinferreira.expenses.database.DatabaseHelper.Expense.DESCRIPTION;
+import static luankevinferreira.expenses.database.DatabaseHelper.Expense.EXPENSE_DATE;
+import static luankevinferreira.expenses.database.DatabaseHelper.Expense.TABLE;
+import static luankevinferreira.expenses.database.DatabaseHelper.Expense.TYPE;
+import static luankevinferreira.expenses.database.DatabaseHelper.Expense.VALUE;
+import static luankevinferreira.expenses.database.DatabaseHelper.Expense._ID;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATA_BASE_NAME = "expenses";
+    private static final int VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATA_BASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE + " (" + _ID + " INTEGER PRIMARY KEY, " +
+                DESCRIPTION + " TEXT, " + VALUE + " DOUBLE," + EXPENSE_DATE
+                + " DATETIME DEFAULT CURRENT_TIMESTAMP, " + TYPE + " TEXT DEFAULT \"Undefined\");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    public static class Expense {
+        public static final String TABLE = "expense";
+        public static final String _ID = "_id";
+        public static final String DESCRIPTION = "description";
+        public static final String VALUE = "value";
+        public static final String EXPENSE_DATE = "expense_date";
+        public static final String TYPE = "expense_type";
+    }
+}
