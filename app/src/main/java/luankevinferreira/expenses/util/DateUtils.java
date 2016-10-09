0b3bd44cee122dbe85badc217337b3f2d2a6c4ca
Luan Kevin Ferreira <luankevinferreira@gmail.com>
2016-10-09 16:25:35 -0300

    Codacy adjusts.

--- app/src/main/java/luankevinferreira/expenses/util/DateUtils.java
+++ app/src/main/java/luankevinferreira/expenses/util/DateUtils.java
@@ -7,20 +7,21 @@ import java.util.Locale;
 public class DateUtils {
 
     public static final int ONE_MONTH = 1;
-    public static final int FOUR_MONTH = 4;
-    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
-    public SimpleDateFormat dateFormat;
+    static final int FOUR_MONTH = 4;
+    private static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    private SimpleDateFormat dateFormat;
 
     public DateUtils() {
         dateFormat = new SimpleDateFormat(YYYY_MM_DD_HH_MM_SS, Locale.getDefault());
     }
 
     public String getStringDateTime(Date date) {
-        if (date == null) {
-            date = new Date();
+        Date localDate = date;
+        if (localDate == null) {
+            localDate = new Date();
         }
 
-        return dateFormat.format(date);
+        return dateFormat.format(localDate);
     }
 
     public SimpleDateFormat getDateFormat() {
