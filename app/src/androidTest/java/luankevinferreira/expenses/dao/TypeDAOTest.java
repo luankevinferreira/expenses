fc7435090bbd208e93073d78f591ded6310552bb
Luan Kevin Ferreira <luankevinferreira@gmail.com>
2018-01-28 16:10:59 -0200

    Added order by for expense types.

--- app/src/androidTest/java/luankevinferreira/expenses/dao/TypeDAOTest.java
+++ app/src/androidTest/java/luankevinferreira/expenses/dao/TypeDAOTest.java
@@ -6,22 +6,16 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.List;
 
-import luankevinferreira.expenses.domain.Type;
-
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
 
-/**
- * Created by Luan Kevin Ferreira.
- */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class TypeDAOTest {
@@ -46,7 +40,7 @@ public class TypeDAOTest {
     @Test
     public void testFindAllDescriptions() throws Exception {
         // Prepare
-        String expectedName = "Food";
+        String expectedName = "Clothes";
 
         // Action
         List<String> descriptions = typeDAO.findAllDescriptions();
@@ -55,5 +49,21 @@ public class TypeDAOTest {
         assertNotNull(descriptions);
         assertFalse(descriptions.isEmpty());
         assertEquals(expectedName, descriptions.get(0));
+    }
+
+    @Test
+    public void testOrderBy() {
+        // Prepare
+        List<String> descriptions;
+
+        // Action
+        descriptions = typeDAO.findAllDescriptions();
+
+        // Verify
+        assertNotNull(descriptions);
+        assertFalse(descriptions.isEmpty());
+        assertEquals("Clothes", descriptions.get(0));
+        assertEquals("Dwelling", descriptions.get(1));
+        assertEquals("Education", descriptions.get(2));
     }
 }
